773a5d05029c9f9a9aed71900fd105400179f455
Bizwayo <39114620+Bizwayo@users.noreply.github.com>
2023-02-13 21:13:19 -0600

    Front End Added - Copied

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ require('dotenv').config();
 
 
 //middleware
+app.use(express.static('./public'))
 app.use(express.json());
 
 //routes
-app.get('/hello', (req,res)=>{
-    res.send("Task Manager");
-})
-
 app.use('/api/v1/tasks', tasks)
 
 //app.get('/api/v1/tasks')
